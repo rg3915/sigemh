8991f3038c4ea8e3fe22c7c30f462f84e7b0ce57
Victor Acioly <Victor Acioly>
2017-05-10 13:41:34 -0300

    Primero Add do SIGEMH no Git

---
+++ core/views.py
@@ -0,0 +1,16 @@
+# coding=utf-8
+
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def contact(request):
+    return render(request, 'contact.html')
+
+
+def product(request):
+    return render(request, 'product.html')
